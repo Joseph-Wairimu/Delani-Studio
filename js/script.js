16bef48290dbfd62a5da3980bbf3fa43ea7d5591
Joseph-Wairimu <josephkamore084@gmail.com>
2021-12-19 20:10:20 +0300

    adding hover to portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// tongle
 $(document).ready(function() {
     $(".icon").click(function() {
       $(".first").toggle();
@@ -18,3 +19,69 @@ $(document).ready(function() {
       $(".tongleee").toggle();
     });
   });
+
+
+// Hover
+
+  $(document).ready(function(){
+    $(".hover").hover(function(){
+      $(".hover").css("opacity", "0.5");
+      }, function(){
+      $(".delani").css("display", "flex");
+    });
+
+    $(".ho").hover(function(){
+        $(".ho").css("opacity", "0.5");
+        }, function(){
+        $(".delani").css("display", "flex");
+      });
+
+      $(".hov").hover(function(){
+        $(".hov").css("opacity", "0.5");
+        }, function(){
+        $(".delani").css("display", "flex");
+      });
+
+      $(".hove").hover(function(){
+        $(".hove").css("opacity", "0.5");
+        }, function(){
+        $(".delani").css("display", "flex");
+      });
+
+
+      $(".hoo").hover(function(){
+        $(".hoo").css("opacity", "0.5");
+        }, function(){
+        $(".delani").css("display", "flex");
+      });
+
+      $(".hovv").hover(function(){
+        $(".hovv").css("opacity", "0.5");
+        }, function(){
+        $(".delani").css("display", "flex");
+      });
+
+      $(".hovee").hover(function(){
+        $(".hovee").css("opacity", "0.5");
+        }, function(){
+        $(".delani").css("display", "flex");
+      });
+
+      $(".hoverr").hover(function(){
+        $(".hoverr").css("opacity", "0.5");
+        }, function(){
+        $(".delani").css("display", "flex");
+      });
+
+
+
+
+
+
+
+
+
+
+  });
+  
+
